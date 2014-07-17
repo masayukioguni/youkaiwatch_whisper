c796b5a231333a8084b16217b276838ad1b21b61
masayuki_oguni <masayuki_oguni@dwango.co.jp>
2014-07-17 19:25:45 +0900

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name youkaiwatchWhisperApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the youkaiwatchWhisperApp
+ */
+angular.module('youkaiwatchWhisperApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name youkaiwatchWhisperApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the youkaiwatchWhisperApp
+ */
+angular.module('youkaiwatchWhisperApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
