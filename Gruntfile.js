0f36b74222edf1195716c8f0ffe12bf373955f5b
masayuki_oguni <masayuki_oguni@dwango.co.jp>
2014-07-17 19:32:27 +0900

    add deploy script

--- Gruntfile.js
+++ Gruntfile.js
@@ -388,6 +388,21 @@ module.exports = function (grunt) {
         configFile: 'test/karma.conf.js',
         singleRun: true
       }
+    },
+
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:masayukioguni/youkaiwatch_whisper.git',
+          branch: 'gh-pages'
+        }
+      }
     }
   });
 
